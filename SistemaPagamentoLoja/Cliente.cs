397c5ab838c3af1446cf6543fe9ceb0f79eb5b6a
espinola2007 <espinolavitoria3@gmail.com>
2026-08-22 16:44:14 -0300

    Add project files.

---
+++ SistemaPagamentoLoja/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPagamentoLoja
+{
+    internal class Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; }
+
+        public Cliente(string nome, string cpf)
+        {
+            Nome = nome;
+            CPF = cpf;
+        }
+
+    }
+}
---
+++ SistemaPagamentoLoja/Program.cs
@@ -0,0 +1,6 @@
+﻿using SistemaPagamentoLoja;
+
+Cliente cliente1 = new Cliente("Maria", "1123476509889");
+
+Console.WriteLine($"Nome: {cliente1.Nome}");
+Console.WriteLine($"CPF: {cliente1.CPF}");
